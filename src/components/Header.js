6db3cf0ded582d0cab4d1306182729c0c2d4e69c
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2022-12-07 12:19:23 +0100

    pushing to Github Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+
+import trollFace from "../images/troll-face.png"
+
+
+
 function Header() {
   return (
     <header className="header">
-      <img src="./images/troll-face.png" alt="" className="header--image" />
+      <img src={trollFace} alt="" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
